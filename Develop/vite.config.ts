87f404632803c1564ea5957f6a3cba4fc4b15039
KyBrMar12 <KyBrMar@gmail.com>
2025-01-28 23:25:21 -0600

    modifying vite config file for allowedHosts array

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 10000,
-    allowedHosts: "candidate-search-kyle-style.onrender.com",
+    allowedHosts: ["candidate-search-kyle-style.onrender.com"],
   }
 });
