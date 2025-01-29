58958b8d618f9e8f3c907ba51959db8c33485f55
KyBrMar12 <KyBrMar@gmail.com>
2025-01-28 22:19:33 -0600

    Adding the Nav component for the application

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,35 @@
-const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
+  import { NavLink } from 'react-router-dom';
 
-export default Nav;
+  const Nav = () => {
+    const navStyle = {
+      display: 'flex',
+      gap: '1rem',
+      padding: '1rem',
+      backgroundColor: '#1a1a1a',
+    };
+  
+    const activeStyle = {
+      fontWeight: 'bold',
+      color: 'coral',
+    };
+  
+    return (
+      <nav style={navStyle}>
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/SavedCandidates"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Potential Candidates
+        </NavLink>
+      </nav>
+    );
+  };
+  
+  export default Nav;
